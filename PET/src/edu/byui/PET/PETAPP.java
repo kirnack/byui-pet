8104c61d30a3c56602a9b4ec3a12ef1573201e9e
kirnack.bryon@gmail.com <kirnack.bryon@gmail.com@79526074-0dd5-5783-2740-d22ab074aca9>
2011-11-23 02:53:45 +0000

    Netbeans Initial import.

---
+++ PET/src/edu/byui/PET/PETAPP.java
@@ -0,0 +1,45 @@
+/*
+ * PETAPP.java
+ */
+
+package edu.byui.PET;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class PETAPP extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new PETAPPView());
+        
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of PETAPP
+     */
+    public static PETAPP getApplication() {
+        return Application.getInstance(PETAPP.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(PETAPP.class, args);
+    }
+}
---
+++ PET/src/edu/byui/PET/images/ImagePanel.java
@@ -0,0 +1,103 @@
+package edu.byui.PET.images;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+import java.net.URL;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.image.BufferedImage;
+
+
+/**
+ *
+ *
+ **/
+public class ImagePanel
+    extends JPanel
+{
+    /**
+     * Holds the Wanted background Image
+     **/
+    private ImageIcon image;
+
+    /**
+     * Holds the prefered size of the image
+     **/
+    private Dimension size;
+
+
+    /**
+     * Holds background Location
+     **/
+    private String backLoc;
+
+    public ImagePanel()
+    {
+        this(ClassLoader.getSystemResource("edu/byui/PET/resources/plateImageHolder.jpg"));
+    }
+    
+    /**
+     * Creates a new BackgroundPanel
+     **/
+    public ImagePanel(URL pURL)
+    {
+        //These lines of code are absolutly nessisary to resize the image properly
+        addComponentListener(new ComponentListener(){
+        public void componentHidden(ComponentEvent e){}
+        public void componentMoved(ComponentEvent e){}
+        public void componentShown(ComponentEvent e){}
+        public void componentResized(ComponentEvent e){
+        resizeImage(getWidth(),getHeight());
+        }
+        });
+	try
+	{
+	    image = new ImageIcon(pURL);
+        }
+	catch(Exception e)
+	{
+	    e.printStackTrace();
+	}
+    }
+    
+    /**
+     * Overrides the paintComponent to display the background Image
+     **/
+    protected void paintComponent(Graphics g)
+    {
+	super.paintComponent(g);
+        //System.err.println("Paniting");
+	g.drawImage(image.getImage(), 0, 0, null);
+    }
+
+    /**
+     * changes the image icon to the specificed address in the config file
+     **/
+    public void getImageLoc()
+    {
+            size = getSize();
+	    /**
+	     * resizes the image to the specified screen height and width
+	     **/ 
+	    Image img = image.getImage();
+	    Image newImg = img.getScaledInstance(size.width, size.height, java.awt.Image.SCALE_SMOOTH);
+	    image = new ImageIcon(newImg);
+    }	
+    public void resizeImage(int newW,int newH)
+    {
+        int w = image.getIconWidth();
+        int h = image.getIconHeight();
+        Image oldImage = image.getImage();
+        BufferedImage img = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = img.createGraphics();
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.drawImage(oldImage,0,0,newW,newH,0,0,w,h,null);
+        g.dispose();
+        image.setImage(img);   
+    }
+   }
