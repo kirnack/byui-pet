a00eadc47cbc4a302a13ab1072dbea66bc97fa09
kirnack.bryon@gmail.com <kirnack.bryon@gmail.com@79526074-0dd5-5783-2740-d22ab074aca9>
2011-11-30 20:42:12 +0000

    Have JNICamera Classes and JNI header made. Also have made a dylib that works on my machine.

---
+++ trunk/PET/src/edu/byui/PET/images/CameraImagePanel.java
@@ -0,0 +1,112 @@
+package edu.byui.PET.images;
+
+import edu.byui.PET.camera.Camera;
+import edu.byui.PET.camera.PETProcCamera;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import java.awt.image.*;
+import java.awt.Image;
+
+/**
+ * 
+ * @author kirnack
+ */
+@SuppressWarnings("serial")
+public class CameraImagePanel extends ImagePanel
+{
+
+   private Camera cam;
+
+   /**
+    * 
+    * @param pProc
+    * @param colorType
+    * @throws Exception
+    */
+   public CameraImagePanel(String pProc, int colorType) throws Exception
+   {
+      this(new PETProcCamera(pProc, colorType));
+      
+   }
+   
+   public CameraImagePanel(Camera pCam)  throws Exception
+   {
+      cam = pCam;
+      cam.captureImage();
+      image = new ImageIcon(cam.getImage());
+   }
+
+   /**
+    * 
+    * @return
+    */
+   public boolean writeFile()
+   {
+      return writeFile("Converted");
+   }
+
+   /**
+    * 
+    * @param pName
+    * @return
+    */
+   public boolean writeFile(String pName)
+   {
+      return cam.saveJPEG(pName);
+   }
+
+   /**
+    * 
+    * @param newW
+    * @param newH
+    */
+   @Override
+   public void resizeImage(int newW, int newH)
+   {
+      try
+      {
+         //resets the image to the original
+         image = new ImageIcon(cam.getImage());
+      }
+      catch (Exception ex)
+      {
+         Logger.getLogger(CameraImagePanel.class.getName()).
+                 log(Level.SEVERE, null, ex);
+      }
+      super.resizeImage(newW, newH);
+   }
+
+   /**
+    * 
+    * @throws Exception
+    */
+   public void capture() throws Exception
+   {
+      cam.captureImage();
+      int w = image.getIconWidth();
+      int h = image.getIconHeight();
+      resizeImage(w, h);
+      repaint();
+   }
+
+   /**
+    * 
+    * @return
+    * @throws Exception
+    */
+   public Image getImage() throws Exception
+   {
+      return getBufferedImage();
+   }
+
+   /**
+    * 
+    * @return
+    * @throws Exception
+    */
+   public BufferedImage getBufferedImage() throws Exception
+   {
+      return cam.getImage();
+   }
+}
