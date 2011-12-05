54658bebc73a9225ca41f893d7471bca13d4ca71
atk05003@gmail.com <atk05003@gmail.com@79526074-0dd5-5783-2740-d22ab074aca9>
2011-12-05 18:01:20 +0000

    Added testing code for JNI.

---
+++ trunk/PET/src/edu/byui/PET/gps/GPSTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.byui.PET.gps;
+
+/**
+ *
+ * @author Kyle
+ */
+public class GPSTest
+{
+   public static void main(String[] arg)
+   {
+      System.out.println(new GPS().getGPSString());
+   }
+}
