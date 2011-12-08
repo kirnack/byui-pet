6105ddede0e2a093884c509e6436948202bc0996
matterpalm@gmail.com <matterpalm@gmail.com@79526074-0dd5-5783-2740-d22ab074aca9>
2011-12-08 00:11:41 +0000

    Added GPS access code for gui

--- PET/src/edu/byui/PET/gps/GPSTest.java
+++ PET/src/edu/byui/PET/gps/GPSTest.java
@@ -13,5 +13,21 @@ public class GPSTest
    public static void main(String[] arg)
    {
       System.out.println(new GPS().getGPSString());
+      
+      /*
+      string deg = '?';
+      string min = '?';
+      string space = ' ';
+      string buffer;
+      string justLocation;
+      GPS myGPS = new GPS();
+      buffer = myGPS.getGPSString();
+      justLocation = buffer[16] + buffer[17] + deg  + space + buffer[18] + 
+      buffer[19] + buffer[20] + buffer[21] + buffer[22] + buffer[23]
+      + buffer[24] + min + buffer[26] + space + buffer[28] + buffer[29] + 
+      buffer[30] + deg + space + buffer[31] + buffer[32] + buffer[33] + 
+      buffer[34] + buffer[35] + buffer[36] + buffer[37] + min + buffer[39];
+      */
+      
    }
 }
